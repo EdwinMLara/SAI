d2a7216088dcabeaa51a6c42fc3ec0fed0db113b
Jose Torres <33torresdev@gmail.com>
2025-06-02 11:31:32 -0600

    feat: Add functionality to fetch a product from the database

---
+++ backend/src/controllers/Product.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import logger from '../utils/logger';
+
+import ProductDocument from '../models/Product.model';
+
+const getProduct = async (req: Request, res: Response, next: any) => {
+  const { key } = req.query;
+
+  try {
+    const product = await ProductDocument.findOne({ key });
+
+    if (!product) {
+      res.status(404).json({
+        message: 'Producto no encontrado',
+      });
+    }
+    res.status(200).json({
+      message: 'Producto encontrado',
+      product: product?.toObject(),
+    });
+  } catch (error) {
+    logger.error('Error al obtener el producto:', error);
+    res.status(500).json({
+      status: 500,
+      message: 'Internal Server Error. Could not get product.',
+    });
+  }
+};
+
+export default getProduct;
--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import createProduct from '../controllers/ProductsDB.controller';
 import createInvoice from '../controllers/Invoice.controller';
+import getProduct from '../controllers/Product.controller';
 
 const router = express.Router();
 
 router.post('/products', createProduct);
 router.post('/invoice/new', createInvoice);
+router.post('/get-product', getProduct);
 
 export default router;
