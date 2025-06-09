10e3a092a6399f58291c66e6b1b9c725a19d5e67
Jose Torres <33torresdev@gmail.com>
2025-06-09 13:11:25 -0600

    feat: Add multipart/form-data support in request middleware

--- backend/src/middlewares/Request.middleware.ts
+++ backend/src/middlewares/Request.middleware.ts
@@ -1,30 +1,40 @@
 import { Request, Response, NextFunction } from 'express';
+import logger from '../utils/logger';
 
 const RequestMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ): void => {
-  if (!req.is('application/json')) {
+  if (req.is('application/json') || req.is('multipart/form-data')) {
+    logger.debug(
+      `[Request] Accepted content type: ${req.headers['content-type']}`
+    );
+
+    const methods = ['POST', 'PUT', 'PATCH'];
+    if (methods.includes(req.method)) {
+      if (!req.body || Object.keys(req.body).length === 0) {
+        logger.error(`[Request] Empty body in ${req.method} request.`);
+        res.status(400).json({
+          status: 400,
+          message: 'Bad Request. Request body cannot be empty.',
+        });
+        return;
+      }
+    }
+
+    next();
+  } else {
+    logger.error(
+      `[Request] Unsupported content type: ${req.headers['content-type']}`
+    );
+
     res.status(415).json({
       status: 415,
-      message: 'Unsupported Media Type. Please send JSON data.',
+      message: 'Unsupported Media Type in this request.',
     });
     return;
   }
-
-  const methods = ['POST', 'PUT', 'PATCH'];
-  if (methods.includes(req.method)) {
-    if (!req.body || Object.keys(req.body).length === 0) {
-      res.status(400).json({
-        status: 400,
-        message: 'Bad Request. Request body cannot be empty.',
-      });
-      return;
-    }
-  }
-
-  next();
 };
 
 export default RequestMiddleware;
