9a2d5484fb04127137d7f6e82facd61fd68b6d39
Jose Torres <33torresdev@gmail.com>
2025-06-13 14:57:18 -0600

    chore: Database changes on exceptions

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,14 +2,13 @@ import mongoose from 'mongoose';
 import logger from '../utils/logger';
 
 const connectDB = async () => {
+  if (!process.env.MONGO_URI) {
+    logger.warn('[Mongo] There is an issue with the environment variables.');
+    process.exit(1);
+  }
   try {
-    if (!process.env.MONGO_URI) {
-      logger.warn('[Mongo] There is an issue with the environment variables.');
-      process.exit(1);
-    }
-
     await mongoose.connect(process.env.MONGO_URI);
-    logger.info('[Mongo] Connected');
+    logger.info('[Mongo] Connected Successfully');
   } catch (error) {
     logger.error('[Mongo] DB connection failed', error);
     process.exit(1);
--- backend/src/config/supabase.ts
+++ backend/src/config/supabase.ts
@@ -1,14 +1,26 @@
 import { createClient } from '@supabase/supabase-js';
+import logger from '../utils/logger';
+import dotenv from 'dotenv';
 
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
-  throw new Error(
-    '[Supabase] There is an issue with the environment variables.'
-  );
-}
+dotenv.config();
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
+const connectSupabase = () => {
+  if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
+    logger.warn('[Supabase] There is an issue with the environment variables.');
+    process.exit(1);
+  }
 
-export default supabase;
+  try {
+    const supabase = createClient(
+      process.env.SUPABASE_URL,
+      process.env.SUPABASE_KEY
+    );
+    logger.info('[Supabase] Connected Successfully');
+    return supabase;
+  } catch (error) {
+    logger.error('[Supabase] Connection failed', error);
+    process.exit(1);
+  }
+};
+
+export default connectSupabase;
