01510429e55c93cff3c07a5e9615f509d9bad8fc
Jose Torres <33torresdev@gmail.com>
2025-06-16 22:50:16 -0600

    feat(stable): implement graceful shutdown process

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import logger from '../utils/logger';
 
-const connectDB = async () => {
+export const connect = async () => {
   if (!process.env.MONGO_URI) {
     logger.warn('[Mongo] There is an issue with the environment variables.');
     process.exit(1);
@@ -15,4 +15,12 @@ const connectDB = async () => {
   }
 };
 
-export default connectDB;
+export const disconnect = async () => {
+  try {
+    await mongoose.disconnect();
+    logger.info('[Mongo] Disconnected Successfully');
+  } catch (error) {
+    logger.error('[Mongo] Disconnection failed', error);
+    process.exit(1);
+  }
+};
