d87b66c596b1ac98e8051beed3ce302206b8c7b9
Jose Torres <33torresdev@gmail.com>
2025-06-19 11:20:03 -0600

    refactor(documents): separate CRUD responsibilities into service pattern

--- backend/src/controllers/Document.controller.ts
+++ backend/src/controllers/Document.controller.ts
@@ -1,14 +1,225 @@
-// import logger from '../utils/logger';
-// import supabase from '../config/supabase';
-// import {
-//   DocumentResponse,
-//   DocumentRequest,
-// } from '../interfaces/Document.interfaces';
-// import { Response } from 'express';
+import { Request, Response } from 'express';
 
-// const bucket = process.env.BUCKET_NAME || 'documents';
+import * as documentService from '../services/Document.services';
+import * as documentValidations from './validations/Document.validations';
+import * as invoiceValidations from './validations/Invoice.validations';
 
-// const saveDocument = async (
-//   req: DocumentRequest,
-//   res: Response<DocumentResponse>
-// ): Promise<void> => {};
+import logger from '../utils/logger';
+
+interface MulterRequest extends Request {
+  file?: Express.Multer.File;
+}
+
+export async function createDocumentURL(
+  req: MulterRequest,
+  res: Response
+): Promise<void> {
+  if (!req.file || req.file.mimetype !== 'application/pdf') {
+    res.status(415).json({
+      message: 'The request does not contain the required file type.',
+    });
+    return;
+  }
+
+  try {
+    const validate = await documentValidations.integrity(
+      req.query.id as string
+    );
+
+    if (validate.error) {
+      res.status(400).json({ message: validate.message });
+      return;
+    }
+
+    if (!validate.pass) {
+      res.status(404).json({ message: validate.message });
+      return;
+    }
+
+    const id = req.query.id;
+    const filename = `document_${id}.pdf`;
+
+    const checking = await documentService.search(filename);
+
+    if (checking) {
+      res.status(409).json({
+        message: 'The invoice already contains a stored document',
+      });
+      return;
+    }
+
+    const generate = await documentService.generateURL(req.file, filename);
+
+    res.status(200).json({
+      message: 'The url was successfully generated',
+      url: generate.url,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message:
+        error instanceof Error ? error.message : 'An unknown error occurred',
+    });
+  }
+}
+
+export async function readDocumentURL(
+  req: Request,
+  res: Response
+): Promise<void> {
+  if (!req.query.id) {
+    res.status(400).json({
+      message: 'Bad Request. Invoice id is required.',
+    });
+    return;
+  }
+
+  try {
+    const exists = await invoiceValidations.exists(req.query.id as string);
+
+    if (exists.error) {
+      res.status(500).json({
+        message: exists.message,
+      });
+      return;
+    }
+
+    if (!exists.pass) {
+      res.status(404).json({
+        message: exists.message,
+      });
+      return;
+    }
+
+    const id = req.query.id;
+    const filename = `document_${id}.pdf`;
+
+    const checking = await documentService.search(filename);
+
+    if (!checking) {
+      res.status(404).json({
+        message: 'The document does not exist.',
+      });
+      return;
+    }
+
+    const request = await documentService.searchURL(filename);
+    res.status(200).json({ message: 'URL found', url: request.url });
+  } catch (error) {
+    res.status(500).json({
+      message:
+        error instanceof Error ? error.message : 'An unknown error occurred',
+    });
+  }
+}
+
+export async function updateDocument(
+  req: MulterRequest,
+  res: Response
+): Promise<void> {
+  if (!req.file || req.file.mimetype !== 'application/pdf') {
+    res.status(415).json({
+      message: 'The request does not contain the required file type.',
+    });
+    return;
+  }
+
+  if (!req.query.id) {
+    res.status(400).json({
+      message: 'Bad Request. Invoice id is required.',
+    });
+    return;
+  }
+
+  try {
+    const exists = await invoiceValidations.exists(req.query.id as string);
+
+    if (exists.error) {
+      res.status(500).json({
+        message: exists.message,
+      });
+      return;
+    }
+
+    if (!exists.pass) {
+      res.status(404).json({
+        message: exists.message,
+      });
+      return;
+    }
+
+    const id = req.query.id;
+    const filename = `document_${id}.pdf`;
+
+    const checking = await documentService.search(filename);
+
+    if (!checking) {
+      res.status(404).json({
+        message: 'The document does not exist.',
+      });
+      return;
+    }
+
+    const request = await documentService.updateFile(req.file, filename);
+    res
+      .status(200)
+      .json({ message: 'Document successfully updated', url: request.url });
+  } catch (error) {
+    logger.error(error);
+
+    res.status(500).json({
+      message:
+        error instanceof Error ? error.message : 'An unknown error occurred',
+    });
+  }
+}
+
+export async function deleteDocument(
+  req: Request,
+  res: Response
+): Promise<void> {
+  if (!req.query.id) {
+    res.status(400).json({
+      message: 'Bad Request. Invoice id is required.',
+    });
+    return;
+  }
+
+  try {
+    const exists = await invoiceValidations.exists(req.query.id as string);
+
+    if (exists.error) {
+      res.status(500).json({
+        message: exists.message,
+      });
+      return;
+    }
+
+    if (!exists.pass) {
+      res.status(404).json({
+        message: exists.message,
+      });
+      return;
+    }
+
+    const id = req.query.id;
+    const filename = `document_${id}.pdf`;
+
+    const checking = await documentService.search(filename);
+
+    if (!checking) {
+      res.status(404).json({
+        message: 'The document does not exist.',
+      });
+      return;
+    }
+
+    await documentService.deleteFile(filename);
+    res.status(200).json({ message: 'Document successfully deleted' });
+  } catch (error) {
+    logger.error(error);
+    res.status(500).json({
+      message:
+        error instanceof Error ? error.message : 'An unknown error occurred',
+    });
+  }
+}
---
+++ backend/src/controllers/validations/Document.validations.ts
@@ -0,0 +1,36 @@
+import { exists } from './Invoice.validations';
+
+interface Response {
+  pass: boolean;
+  message: string;
+  error?: boolean;
+}
+
+export async function integrity(query: string): Promise<Response> {
+  if (!query) {
+    return {
+      pass: false,
+      message: 'Invoice ID is missing in the query request',
+      error: true,
+    };
+  }
+
+  try {
+    const invoice = await exists(query);
+
+    return {
+      pass: invoice.pass,
+      message: invoice.message,
+      error: invoice.error,
+    };
+  } catch (error) {
+    console.error('An error occurred during integrity check:', error);
+
+    return {
+      pass: false,
+      message:
+        error instanceof Error ? error.message : 'An unknown error occurred',
+      error: true,
+    };
+  }
+}
