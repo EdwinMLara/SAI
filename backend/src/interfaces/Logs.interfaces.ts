7cb2968eb394e391a301888f94de74526d56be8e
Jose Torres <33torresdev@gmail.com>
2025-07-18 13:52:08 -0600

    feat(logger): add new logger to store logs in the database log storage

---
+++ backend/src/interfaces/Logs.interfaces.ts
@@ -0,0 +1,19 @@
+import { Document } from 'mongoose';
+
+export interface LogMetadata {
+  from: string;
+  user: string;
+  clientIp: string;
+  stackTrace: string;
+  endpoint: string;
+  method: string;
+  statusCode: number;
+}
+
+export interface LogInterface extends Document {
+  timestamp: Date;
+  level: string;
+  message: string;
+  origin: string;
+  metadata?: LogMetadata;
+}
---
+++ backend/src/utils/Logger.utils.ts
@@ -0,0 +1,91 @@
+import winston from 'winston';
+import Transport from 'winston-transport';
+import { LogMetadata } from '../interfaces/Logs.interfaces';
+
+import LogModel from '../models/Logs.model';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+let logger: winston.Logger;
+
+if (isProduction) {
+  class MongoTransport extends Transport {
+    log(info: any, callback: () => void) {
+      setImmediate(() => this.emit('logged', info));
+      const { level, message, origin, metadata, timestamp } = info;
+      if (!metadata) return callback();
+      let ts: Date;
+      if (timestamp instanceof Date) {
+        ts = timestamp;
+      } else if (typeof timestamp === 'string') {
+        ts = new Date(timestamp);
+      } else {
+        ts = new Date();
+      }
+      let meta = { ...metadata };
+      if (!meta.stackTrace || meta.stackTrace === '') {
+        meta.stackTrace = '-';
+      }
+      const log = new LogModel({
+        timestamp: ts,
+        level,
+        message,
+        origin,
+        metadata: meta,
+      });
+      log.save();
+    }
+  }
+
+  logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format((info) => {
+        if (!info.timestamp) info.timestamp = new Date().toISOString();
+        return info;
+      })(),
+      winston.format.json()
+    ),
+    transports: [new MongoTransport()],
+  });
+} else {
+  logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.printf(({ timestamp, level, message, origin }) => {
+        return `${timestamp} [${level}] (${origin}): ${message}`;
+      })
+    ),
+    transports: [new winston.transports.Console()],
+  });
+}
+
+type LogBase = {
+  level: string;
+  message: string;
+  origin: string;
+};
+
+type LogInput = LogBase & { metadata?: LogMetadata };
+
+function log({ level, message, origin, metadata }: LogInput) {
+  if (isProduction) {
+    if (!metadata) return;
+    logger.log({
+      level,
+      message,
+      origin,
+      metadata,
+    });
+  } else {
+    logger.log({
+      level,
+      message,
+      origin,
+    });
+  }
+}
+
+export default log;
