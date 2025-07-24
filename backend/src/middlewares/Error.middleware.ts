eea4d77f28c0524d2e0175e8cb22eb7557c20607
Jose Torres <33torresdev@gmail.com>
2025-07-24 09:47:54 -0600

    feat(error): log errors and send custom messages in middleware

--- backend/src/middlewares/Error.middleware.ts
+++ backend/src/middlewares/Error.middleware.ts
@@ -1,13 +1,34 @@
-import { Request, Response, NextFunction } from 'express';
-import AppError from '@utils/AppError';
+import { Request, Response } from 'express';
+import AppError from '../utils/AppError';
+import log from '@utils/Logger.utils';
 
-const ErrorMiddleware = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  res.status(500).json({ message: 'Error interno del servidor' });
+const ErrorMiddleware = (err: Error, req: Request, res: Response) => {
+  const returnCode = err instanceof AppError ? err.statusCode || 500 : 500;
+
+  log({
+    level: 'error',
+    message: err.message,
+    origin: 'Middleware errors',
+    metadata: {
+      from: '-',
+      user: req.user?.name || 'Undefined',
+      clientIp: req.ip as string,
+      stackTrace: err.stack as string,
+      endpoint: req.path,
+      method: req.method,
+      statusCode: returnCode,
+    },
+  });
+
+  if (err instanceof AppError) {
+    res.status(err.statusCode || 500).json({
+      message: err.message,
+    });
+    return;
+  }
+  res.status(500).json({
+    message: 'Error interno del servidor',
+  });
 };
 
 export default ErrorMiddleware;
