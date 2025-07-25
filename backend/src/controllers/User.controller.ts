c1d96037920f32583922d0b2b53ec4d1f7c137e6
Jose Torres <33torresdev@gmail.com>
2025-07-25 11:44:58 -0600

    feat(user): set authentication tokens in cookies on user create and update

--- backend/src/controllers/User.controller.ts
+++ backend/src/controllers/User.controller.ts
@@ -3,6 +3,7 @@ import { UserInterface, UserChanges } from '@interfaces/User.interfaces';
 
 import * as helpers from '@helpers/User.helpers';
 import * as services from '@services/User.services';
+import * as cookies from '@utils/cookies/manageCookies';
 
 import responses from '@responses';
 import * as auth from '@auth/crypt';
@@ -25,6 +26,8 @@ export async function createUser(
     req.body.password = await auth.encrypt(req.body.password);
     req.body.role = await helpers.findRole(req.body.email);
     await services.createUser(req.body);
+    await cookies.setAuthToken(res, req.body);
+    await cookies.setRefreshToken(res, req.body);
 
     res.status(201).json({
       message: responses.User.created,
@@ -47,6 +50,7 @@ export async function updateUser(
     const updates: Partial<UserChanges> = req.body;
     await helpers.validateUserChanges(updates);
     const changes = await services.updatedUser(req.user.id, updates);
+    await cookies.setAuthToken(res, changes);
 
     res.status(200).json({
       message: responses.User.updated,
