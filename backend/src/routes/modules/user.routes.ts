5239a46670d19ca2c43bac2247e4a473b5b4fd61
Jose Torres <33torresdev@gmail.com>
2025-07-24 21:29:08 -0600

    refactor(user): remove delete user route and add create user route

--- backend/src/routes/modules/user.routes.ts
+++ backend/src/routes/modules/user.routes.ts
@@ -4,7 +4,7 @@ import * as User from '@controllers/User.controller';
 
 const router = Router();
 
+router.post('/', User.createUser);
 router.put('/', User.updateUser);
-router.delete('/', User.deleteUser);
 
 export default router;
