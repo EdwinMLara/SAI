3cf1083d24b9b219a9be6ac39476cc7386b76248
Jose Torres <33torresdev@gmail.com>
2025-07-28 00:07:48 -0600

    fix(auth): minimize auth token age for improved security

--- backend/src/utils/cookies/manageCookies.ts
+++ backend/src/utils/cookies/manageCookies.ts
@@ -9,10 +9,10 @@ export async function setRefreshToken(
   res: Response,
   user: UserInterface
 ): Promise<void> {
-  const data = await getRefreshData(user);
+  const token = await getRefreshData(user);
 
   try {
-    res.cookie('refreshToken', data, {
+    res.cookie('refreshToken', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
@@ -28,15 +28,15 @@ export async function setAuthToken(
   res: Response,
   user: UserInterface
 ): Promise<void> {
-  const data = await getAuthData(user);
+  const token = await getAuthData(user);
 
   try {
-    res.cookie('accessToken', data, {
+    res.cookie('accessToken', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
       path: '/',
-      maxAge: 24 * 60 * 60 * 1000,
+      maxAge: 60 * 60 * 1000,
     });
   } catch (error) {
     throw error;
