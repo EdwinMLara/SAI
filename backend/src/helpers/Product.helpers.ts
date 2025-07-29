18fab0450ba88f9ce7c55562d7b9296537d0464a
Jose Torres <33torresdev@gmail.com>
2025-07-29 13:18:24 -0600

    refactor(validations): remove old validation controllers and replace with helper modules for Document, Invoice, Product, and Ticket

---
+++ backend/src/helpers/Product.helpers.ts
@@ -0,0 +1,40 @@
+import * as services from '@services/Product.services';
+
+import responses from '@responses';
+import AppError from '@utils/AppError';
+
+/* ------------------ Code ------------------ */
+
+export async function comprobeInexistence(keyProduct: string): Promise<void> {
+  try {
+    const exist = await services.existProduct(keyProduct);
+    if (exist) {
+      throw new AppError(responses.Product.alreadyExists, 409);
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function comprobeExistence(keyProduct: string): Promise<void> {
+  try {
+    const exist = await services.existProduct(keyProduct);
+    if (!exist) {
+      throw new AppError(responses.Product.notFound, 404);
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+export function getQuery(queryParam: any): string {
+  try {
+    const query = queryParam as string;
+    if (!query) {
+      throw new AppError(responses.Product.requiredKey, 400);
+    }
+    return query;
+  } catch (error) {
+    throw error;
+  }
+}
