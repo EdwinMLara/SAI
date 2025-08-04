ec64f5b27c21a6c10a9c032fcd77f121cd731adb
Jose Torres <33torresdev@gmail.com>
2025-08-04 10:49:48 -0600

    chore(logs): add logging to backend index entry point

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,29 @@
 import cors from 'cors';
 import express from 'express';
-
-import logger from '@utils/logger';
-import env from '@utils/env';
+import cookieParser from 'cookie-parser';
 
 import router from '@routes';
 import * as Mongo from '@config/database';
-import cookieParser from 'cookie-parser';
 
+import env from '@utils/env';
+import log from '@utils/Logger.utils';
+
+import ErrorMiddleware from '@middlewares/Error.middleware';
 import RequestMiddleware from '@middlewares/Request.middleware';
 import ResponseMiddleware from '@middlewares/Response.middleware';
-import ErrorMiddleware from '@middlewares/Error.middleware';
+
+/* ------------------ Code ------------------ */
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: env.ORIGIN_SERVER,
+    credentials: true,
+  })
+);
 
 app.use(RequestMiddleware);
 app.use(ResponseMiddleware);
@@ -24,22 +31,34 @@ app.use(ResponseMiddleware);
 Mongo.connect();
 
 app.use('/api', router);
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
 app.use(ErrorMiddleware);
 
 const server = app.listen(Number(env.SERVER_PORT), env.SERVER_IP, () => {
-  logger.info(`[Express] Connected on  ${env.SERVER_IP}:${env.SERVER_PORT}`);
+  log({
+    level: 'debug',
+    message: 'Express server on',
+  });
 });
 
 const gracefulShutdown = async (signal: string) => {
-  logger.info(`[Express] ${signal} received. Shutting down server...`);
-
   server.close(async () => {
     await Mongo.disconnect();
-    logger.info('[Express] Server closed.');
+    log({
+      level: 'debug',
+      message: 'Express server off',
+    });
   });
 
   setTimeout(() => {
-    logger.error('[Express] Forced shutdown due to timeout');
+    log({
+      level: 'debug',
+      message: 'Database connection not closed in time',
+    });
     process.exit(1);
   }, 10000);
 };
