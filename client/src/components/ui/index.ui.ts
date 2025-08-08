d481b157bd26f7b23a58b5800bd653e0a4aca89f
Jose Torres <33torresdev@gmail.com>
2025-08-07 19:49:08 -0600

    Add UI index file for component exports

---
+++ client/src/components/ui/index.ui.ts
@@ -0,0 +1,10 @@
+export { default as Alert } from './Alert';
+export { default as Button } from './Button';
+export { default as CardViewer } from './CardViewer';
+export { default as Icon } from './Icon';
+export { default as IconButton } from './IconButton';
+export { default as Input } from './Input';
+export { default as InputFile } from './InputFile';
+export { default as Modal } from './Modal';
+export { default as Notification } from './Notification';
+export { default as Table } from './Table';
