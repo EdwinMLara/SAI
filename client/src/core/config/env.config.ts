85445602c87edd0f4e5933a5edce08a0b8d8ac21
Jose Torres <33torresdev@gmail.com>
2025-08-22 22:37:37 -0600

    chore(env): remove unused env module

--- client/src/core/config/env.config.ts
+++
@@ -1,7 +0,0 @@
-const env = {
-   NODE_ENV: import.meta.env.MODE || '',
-   VITE_API_HOST: import.meta.env.VITE_API_HOST || '',
-   VITE_API_PORT: import.meta.env.VITE_API_PORT || '',
-};
-
-export default env;
