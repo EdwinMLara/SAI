6531a16d22ddee65b2fda6bf8f177e79220e7a65
Jose Torres <33torresdev@gmail.com>
2025-08-25 21:40:36 -0600

    chore(env): update environment path handling in Vite and config

--- client/src/core/config/env.ts
+++
@@ -1,9 +0,0 @@
-const envOptions = ['NODE_ENV', 'API_HOST', 'API_PORT'];
-
-const env = {
-   NODE_ENV: import.meta.env.VITE_NODE_ENV || 'development',
-   API_HOST: import.meta.env.VITE_API_HOST || '0.0.0.0',
-   API_PORT: import.meta.env.VITE_API_PORT || '5001',
-};
-
-export default env;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
    server: {
       proxy: {
          '/api': {
-            target: `http://${process.env.VITE_API_HOST || 'localhost'}:${process.env.VITE_API_PORT || '5001'}`,
+            target: 'http://localhost:5001',
             changeOrigin: true,
             secure: false,
          },
