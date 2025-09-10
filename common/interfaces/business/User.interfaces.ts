366111653631666264a605e99f2d570052780416
torress-dev <33torresdev@gmail.com>
2025-09-10 13:25:46 -0600

    feat(user): add _id field to User interface

--- common/interfaces/business/User.interfaces.ts
+++ common/interfaces/business/User.interfaces.ts
@@ -1,4 +1,5 @@
 export interface UserInt {
+   _id: string;
    image?: string;
    name: string;
    username: string;
@@ -18,6 +19,7 @@ export interface UserChangesInt {
 }
 
 export interface PublicUserInt {
+   _id: string;
    image?: string;
    name: string;
    username: string;
