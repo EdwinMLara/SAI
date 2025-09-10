4b64f0b4c838a7e69f486ea3174ffc45330b61a0
torress-dev <33torresdev@gmail.com>
2025-09-10 14:05:54 -0600

    refactor(user): update public user info return structure

--- backend/src/helpers/User.helpers.ts
+++ backend/src/helpers/User.helpers.ts
@@ -1,13 +1,8 @@
 import AppError from '@utils/system/AppError';
 import responses from '@utils/system/responses';
 
-import {
-   comprobeUniqueFields,
-   getUserById,
-   userByEmail,
-} from '@services/User.services';
 import { PublicUserInt, UserInt } from '@cmm_interfaces/index';
-import { User } from '@supabase/supabase-js';
+import { comprobeUniqueFields, userByIndexed } from '@services/User.services';
 
 /* ------------------ Code ------------------ */
 
@@ -17,19 +12,24 @@ export async function comprobeFields(
    phone?: string
 ): Promise<string | void> {
    const fields: { username?: string; email?: string; phone?: string } = {};
+
    if (username !== undefined) fields.username = username;
    if (email !== undefined) fields.email = email;
    if (phone !== undefined) fields.phone = phone;
-   const findField = await comprobeUniqueFields(fields);
 
+   const findField = await comprobeUniqueFields(fields);
    if (findField) {
       return `El ${findField} ya está en uso.`;
    }
 }
 
-export async function returnPublicUser(userId: string): Promise<PublicUserInt> {
-   const user = await getUserById(userId);
-   const publicUser: PublicUserInt = {
+export async function returnPublicUser(
+   userIndex: string
+): Promise<PublicUserInt> {
+   const user = await userByIndexed(userIndex);
+
+   return {
+      _id: user._id,
       image: user.image ?? '',
       name: user.name,
       username: user.username,
@@ -37,19 +37,4 @@ export async function returnPublicUser(userId: string): Promise<PublicUserInt> {
       email: user.email,
       role: user.role,
    };
-
-   return publicUser;
-}
-
-export function returnPublicUserByUser(user: UserInt): PublicUserInt {
-   const publicUser: PublicUserInt = {
-      image: user.image ?? '',
-      name: user.name,
-      username: user.username,
-      phone: user.phone,
-      email: user.email,
-      role: user.role,
-   };
-
-   return publicUser;
 }
