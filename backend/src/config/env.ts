e6646ddf775831a156ec664c281905be3d42f13c
torress-dev <33torresdev@gmail.com>
2025-10-01 20:20:27 -0600

    feat(config): add env paths config for environment variable management

---
+++ backend/src/config/env.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env', quiet: true });
+
+const envOptions = [
+   'NODE_ENV',
+   'SERVER_IP',
+   'SERVER_PORT',
+   'ORIGIN_SERVER',
+   'ORIGIN_CLIENT',
+];
+
+envOptions.forEach((key) => {
+   if (!process.env[key]) {
+      throw new Error(`Missing environment variable: ${key}`);
+   }
+});
+
+const env = {
+   NODE_ENV: process.env.NODE_ENV || '',
+   SERVER_IP: process.env.SERVER_IP || '',
+   SERVER_PORT: process.env.SERVER_PORT || '',
+   ORIGIN_SERVER: process.env.ORIGIN_SERVER || '',
+   ORIGIN_CLIENT: process.env.ORIGIN_CLIENT || '',
+};
+
+export default env;
